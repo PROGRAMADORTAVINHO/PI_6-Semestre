25b92b2e7f7a7d802c2f3a8363e9cde312f521ee
Joao_Otavio <jotavio250@gmail.com>
2024-09-21 08:37:29 -0300

    About

--- About/Slide.js
+++ About/Slide.js
@@ -5,4 +5,25 @@ var swiper = new Swiper(".slide-content", {
     centerSlide: 'true',
     fade: 'true',
     grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
 });
